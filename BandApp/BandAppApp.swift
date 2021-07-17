a727ffc1a18764a51094c1e23cc65a755de248da
Takumi Otsuka <takumi.ot0911@gmail.com>
2021-07-17 16:19:10 +0800

    Initial Commit

---
+++ BandApp/BandAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandAppApp.swift
+//  BandApp
+//
+//  Created by Takumi Otsuka on 17/7/21.
+//
+
+import SwiftUI
+
+@main
+struct BandAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
